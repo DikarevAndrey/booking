27d664c3998277a8fac9caa812a4008c33c9d25b
Afsent <37215876+Afsent@users.noreply.github.com>
2018-05-11 18:00:07 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :places
   resources :users
-  resources :offers
+  resources :offers 
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
