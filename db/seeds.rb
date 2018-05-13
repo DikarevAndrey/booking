ac3331e216a946ddee6872c46d425ccbb98ff8b2
DikarevAndrey <dias7707@gmail.com>
2018-05-13 14:27:43 +0300

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+cities_list = [
+  "Москва",
+  "Рязань",
+  "Санкт-Петербург",
+  "Казань",
+  "Нижний Новгород"
+]
+
+cities_list.each do |name|
+  City.create(:name => name)
+end
+
+subways_list = [
+  "Таганская",
+  "Волжская",
+  "Бауманская",
+  "Китай-город",
+  "Арбатская",
+  "Павелецкая"
+]
+
+subways_list.each do |name|
+  Subway.create(:name => name)
+end
+
+kinds_list = [
+  "Бар",
+  "Ресторан",
+  "Пиццерия",
+  "Кальянная",
+  "Паб",
+  "Кафе"
+]
+
+kinds_list.each do |name|
+  Kind.create(:name => name)
+end
+
+cuisines_list = [
+  "Итальянская",
+  "Французская",
+  "Русская",
+  "Суши",
+  "Пицца",
+  "Вегетарианская"
+]
+
+cuisines_list.each do |name|
+  Cuisine.create(:name => name)
+end
